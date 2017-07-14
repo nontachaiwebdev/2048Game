1c804f8fd72d509b4f6a80788cb89e30347180a4
nontachaiwebdev <sutininter@gmail.com>
2017-07-14 20:45:28 +0700

    improve performance

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const secretArchivesLock = (lock, actions) => {
-  return actions.split('').reduce((result, word, index)=> {
+  const listLock = toList(lock)
+  const resultOperateLock = actions.split('').reduce((result, word, index)=> {
     if(word === 'U' || word === 'D'){
       const flip = flipLock(result)
       const operate = operateLock(flip)
@@ -9,49 +10,45 @@ const secretArchivesLock = (lock, actions) => {
       const operate = operateLock(result)
       return operate(word)
     }
-  },lock)
+  },listLock)
+  return toWord(resultOperateLock)
 }
 
 const operateLock = (lock) => (operate) => {
-  return lock.map((row, index) => {
-    const arr = row.split('')
-    const word = arr.reduce((result, point) => {
-      return point !== '.' ? [...result, point] : result
-    },[])
-    const dot = new Array(arr.length - word.length).fill('.').map((string, index) => (string))
-    return (operate === 'L' ? [...word, ...dot] : [...dot, ...word]).join('')
+  return lock.map((point) => {
+    const word = point.join('').split('.').join('').split('')
+    const dot = new Array(point.length - word.length).fill('.')
+    const finalResult = operate === 'L' ? [...word, ...dot] : [...dot, ...word]
+    return finalResult
   })
 }
 
 const flipLock = (lock) => {
-  const arrLock = lock.map((row, index) => {
-    return row.split('')
-  })
-  return arrLock[0].map((col, index) => {
-    const colFlip = arrLock.map((row) => {
+  console.log(lock[0])
+  return lock[0].map((col, index) => {
+    return lock.map((row) => {
       return row[index]
     })
-    return colFlip.join('')
   })
 }
 
+const toList = (lock) => {
+  return lock.map((row, index) => {
+    return row.split('')
+  })
+}
+
+const toWord = (lock) => {
+  return lock.map((row, index) => {
+    return row.join('')
+  })
+}
 
 const input =
-["V...Y..E.PRFI..",
- ".ZO...ZU.R.Z...",
- ".....R.......U.",
- "..N.CE...S..RP.",
- "...J........T..",
- ".......H..I....",
- ".N.JE.......J.F",
- "...............",
- "A.K..G.........",
- "...PW.....W.G..",
- "UO..BJT.G.I....",
- ".R.....J.......",
- "...............",
- "..M.GCD.....S..",
- ".X.S..........."]
+["....",
+ "AB..",
+ ".C..",
+ "...."]
 
-const action = 'LULLR'
+const action = 'D'
 console.log(secretArchivesLock(input, action))
